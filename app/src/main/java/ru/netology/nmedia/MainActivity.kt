a22967b410d18595971c2f0bf0a43c7c8380cb31
krissbel <101643482+krissbel@users.noreply.github.com>
2022-07-06 14:07:31 +0800

    Update MainActivity.kt

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,7 +2,6 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageButton
 import androidx.annotation.DrawableRes
 import ru.netology.nmedia.databinding.ActivityMainBinding
 
@@ -22,21 +21,20 @@ class MainActivity : AppCompatActivity() {
             content = "events",
         )
 
+        binding.render(post)
         binding.like.setOnClickListener {
             post.likedByMe = !post.likedByMe
             val imageResId =
                 if (post.likedByMe) R.drawable.ic_red_like_24dp else R.drawable.ic_like_24dp
             binding.like.setImageResource(imageResId)
-
-            val countLikes =
-                if (imageResId == R.drawable.ic_red_like_24dp) post.likes + 1 else post.likes
-            binding.countLike.text = countLikes.toString()
+            var newCount = post.likes + 1
+            if (post.likedByMe) newCount else newCount -= 1
+            binding.countLike.text = countInTextFormat(newCount)
         }
 
         binding.share.setOnClickListener {
             var countShare = post.countShare++
-            countShare = 100000
-            binding.countShare.text = agoToText(countShare).toString()
+            binding.countShare.text = countInTextFormat(countShare)
 
         }
     }
@@ -47,6 +45,8 @@ private fun ActivityMainBinding.render(post: Post) {
     authorName.text = post.author
     date.text = post.published
     textPost.text = post.content
+    countShare.text = countInTextFormat(post.countShare)
+    countLike.text = countInTextFormat(post.likes)
 
 }
 
@@ -58,21 +58,19 @@ fun convertCount(countShare: Int): Int {
     return countShare / 1000
 }
 
-fun agoToText(countShare: Int): String {
+fun countInTextFormat(count: Int): String {
 
-    if (countShare < 999) return countShare.toString()
+    if (count < 999) return count.toString()
 
-    if ((countShare < 10000) && (countShare > 999))  when {
-        (countShare / 100) % 10 == 0 ->  return convertCount(countShare).toString() + "K"
-        (countShare / 100) % 10 in 1..9 -> return convertCount(countShare).toString() + "." + (countShare / 100) % 10 + "K"
+    if ((count < 10000) && (count > 999)) when {
+        (count / 100) % 10 == 0 -> return convertCount(count).toString() + "K"
+        (count / 100) % 10 in 1..9 -> return convertCount(count).toString() + "." + (count / 100) % 10 + "K"
     }
-    if ((countShare > 9999) && (countShare < 99999)) when {
-        (countShare / 100) % 10 in 0..9 -> return convertCount(countShare).toString() + "K"
+    if ((count > 9999) && (count < 99999)) when {
+        (count / 100) % 10 in 0..9 -> return convertCount(count).toString() + "K"
     }
-    if (countShare > 99999) when {
-        (countShare / 100) % 100 in 0..9 ->  return convertCount(countShare).toString() + "M"
+    if (count > 99999) when {
+        (count / 100) % 100 in 0..9 -> return convertCount(count).toString() + "M"
     }
-return countShare.toString()
+    return count.toString()
 }
-
-
